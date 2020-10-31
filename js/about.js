b8502f153d15a5f2bfb7c78b473e8a9d953f014e
Exterior7 <exteriorbwin@gmail.com>
2020-10-31 18:48:23 +0100

    code refact

--- js/about.js
+++ js/about.js
@@ -8,51 +8,50 @@ var about = about || {};
         return document.querySelector(s);
     };
 
-
-    var validateEmail=function (email)  {
+    // Chacking Email validation
+    var isValidEmail = function (email) {
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
     }
 
     //Name Field Node
     var inpName = getNode('#inpName');
- 
+
     //Email Field Node
     var inpEmail = getNode('#inpEmail');
-   
+
     //Message Field Node
     var inpMessage = getNode('#inpMessage');
-    
+
     // Error field Node
     var errorField = getNode('#errorField');
 
     //Submit button Node
     var btnSaveForm = getNode('#btnSaveForm');
-    
-    btnSaveForm.addEventListener("click", function (e) {
-        
+
+    var sendEmail = function (e) {
 
         // Hiba tömb
-        var arrError=[];
-       
+        var arrError = [];
+
         // clear error field
-        errorField.innerHTML='';
-        
+        errorField.innerHTML = '';
+
         // Chacking Name
-        var inpNameValue=inpName.value;
-        if (inpNameValue== '' || inpNameValue.length < 5) {
+        var inpNameValue = inpName.value;
+        if (inpNameValue == '' || inpNameValue.length < 5) {
             arrError.push('A név mező üres vagy kisebb mint 5 karakter');
         }
 
         // Chacking Email
-        var inpEmailValue=inpEmail.value;
-        if (!validateEmail(inpEmailValue)) {
+        var inpEmailValue = inpEmail.value;
+        if (!isValidEmail(inpEmailValue)) {
             arrError.push('Üres az email mező vagy érvénytelen az email cím');
         }
 
         // Checking Message
-        var inpMessageValue=inpMessage.value;
-        if (inpMessageValue== '') {
+        var inpMessageValue = inpMessage.value;
+        if (inpMessageValue == '') {
             arrError.push('Az üzenet mező nem lehet üres');
         }
         else if (inpMessageValue.length <= 20) {
@@ -64,15 +63,17 @@ var about = about || {};
 
         // Checking Errors
         if (arrError.length > 0) {
-            var Message='<ul><li>'+arrError.join('</li><li>')+'</li></ul>';
-            console.log(Message); 
-            errorField.innerHTML=Message;
-            errorField.classList.remove('errorFieldHide');       
-            errorField.classList.add('errorFieldShow');       
+            var Message = '<ul><li>' + arrError.join('</li><li>') + '</li></ul>';
+            errorField.innerHTML = Message; // add errors msg
+            errorField.classList.remove('errorFieldHide'); // remove Hide class
+            errorField.classList.add('errorFieldShow'); // add Show class
             e.preventDefault();
         }
-        else {return true};
-    },false);
+        else { return true };
+    }
+
+    // Send email on click
+    btnSaveForm.addEventListener("click", sendEmail, false);
 
 
 }(about));
