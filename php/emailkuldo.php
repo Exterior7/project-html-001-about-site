0b37f935017f22e455a237f2fa7acbb9e088805b
Exterior7 <exteriorbwin@gmail.com>
2020-10-31 18:33:59 +0100

    php update

--- php/emailkuldo.php
+++ php/emailkuldo.php
@@ -1,5 +1,11 @@
 <?php
 
-echo '<pre>'. print_r($_POST,true).'</pre>';
+// echo '<pre>'. print_r($_POST,true).'</pre>';
+$inpName=$_POST['inpName'];
+echo $inpName.'<br>';
+$inpEmail=$_POST['inpEmail'];
+echo $inpEmail.'<br>';
+$inpMessage=$_POST['inpMessage'];
+echo $inpMessage.'<br>';
 
 ?>
